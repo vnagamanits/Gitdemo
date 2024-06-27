7cfed59c8748ffbe152ccacdc37de51119dde0c7
vnagamanits <vnagamanits@gmail.com>
2024-06-27 16:19:18 +0200

    Git Demo Message

---
+++ src/TestGitDemo/GitClsDemo.java
@@ -0,0 +1,12 @@
+package TestGitDemo;
+
+public class GitClsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to GIT");
+
+	}
+
+}
